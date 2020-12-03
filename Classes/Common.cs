fd962e8e49984444fe017cde2ae953c5e8562764
EmichiNaoki <67722159+EmichiNaoki@users.noreply.github.com>
2020-12-03 18:05:14 +0900

    first

---
+++ Classes/Common.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+namespace WPFCore.Classes
+{
+    class Common
+    {
+        static Common()
+        {
+
+        }
+
+        public string GetConnectionString()
+        {
+            var config = new ConfigurationBuilder();
+            config.SetBasePath(Directory.GetCurrentDirectory());
+            config.AddJsonFile(path: "appsettings.json");
+            config.AddEnvironmentVariables(prefix: "ConnectionStrings");
+            config.AddUserSecrets<InvalidProgramException>(optional: true);
+            var configuration = config.Build();
+
+
+            //Console.WriteLine(configuration["ConnectionStrings:Toufree"]);
+
+
+            return configuration["ConnectionStrings:Toufree"];
+
+
+
+        }
+    }
+
+}
---
+++ Classes/DBConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+
+
+
+namespace WPFCore.Classes
+{
+    public partial class DBConnection
+    {
+
+
+
+        static DBConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            var cmncls = new Common();
+            builder.ConnectionString = cmncls.GetConnectionString();
+
+
+            try
+            {
+
+
+                // SQL Server に接続
+                Console.Write("SQL Server に接続しています... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("接続成功。");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("すべてが完了しました。任意のキーを押してアプリを終了します...");
+            Console.ReadKey(true);
+
+        }
+
+
+    }
+
+}
+
+    
